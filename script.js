c67255e8e7a28ac16eebc12ba792ff22dacb49f4
Michael Allen <michaelallen@Michaels-Air.attlocal.net>
2021-11-21 19:20:08 -0600

    Finished studio except bonus.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            const astronauts = json;
+            console.log(astronauts);
+            for (let i=0; i<astronauts.length; i++){
+                const container = document.getElementById("container");
+                container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronauts[i].firstName} ${astronauts[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronauts[i].hoursInSpace}</li>
+                            <li>Active: ${astronauts[i].active}</li>
+                            <li>Skills: ${astronauts[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src=${astronauts[i].picture}>
+                </div>
+                `;
+            };
+        });        
+    });
+});
