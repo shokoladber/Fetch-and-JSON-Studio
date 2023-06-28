af31314e399fdedc53240fc2789792310d80e358
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-06-28 13:56:10 -0500

    Updated for TAing 2023.

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 window.addEventListener("load", function(){
-    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-        response.json().then(function(json){
+    const fetchPromise = fetch("https://handlers.education.launchcode.org/static/astronauts.json");
+    fetchPromise.then(function(response){
+        const jsonPromise = response.json();
+        jsonPromise.then(function(json){
             const astronauts = json;
             console.log(astronauts);
+
+
             for (let i=0; i<astronauts.length; i++){
                 const container = document.getElementById("container");
-                container.innerHTML += `
-                <div class="astronaut">
-                    <div class="bio">
-                        <h3>${astronauts[i].firstName} ${astronauts[i].lastName}</h3>
-                        <ul>
-                            <li>Hours in space: ${astronauts[i].hoursInSpace}</li>
-                            <li id="${astronauts[i].id}">Active: ${astronauts[i].active}</li>
-                            <li>Skills: ${astronauts[i].skills}</li>
-                        </ul>
+                    container.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${astronauts[i].firstName} ${astronauts[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${astronauts[i].hoursInSpace}</li>
+                                <li id="${astronauts[i].id}">Active: ${astronauts[i].active}</li>
+                                <li>Skills: ${astronauts[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${astronauts[i].picture}>
                     </div>
-                    <img class="avatar" src=${astronauts[i].picture}>
-                </div>
-                `;
+                    `;
+                
                 if (astronauts[i].active){
                     document.getElementById(`${astronauts[i].id}`).style.color = 'green';
                 };
             };
+
+            const count = document.getElementById("count");
+            count.innerHTML = `<div class="bio">${astronauts.length}</div>`
         });        
     });
 });
